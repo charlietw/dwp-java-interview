a74b760c6508f185e56a231d84268e1c464f9682
Charlie <cwren92@gmail.com>
2020-06-25 22:24:55 +0100

    Add API calls and first pass of calculation for distance

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import requests
+import requests # for easy high level API calls, as recommended in Python documentation
+import math # for sqrt function
 
 
 def get_instructions():
@@ -13,27 +14,57 @@ def get_users():
     """
     Calls API to retrieve all users
     """
-    pass
+    all_users = requests.get('https://bpdts-test-app.herokuapp.com/users')
+    return all_users.json()
+
 
 def get_london_users():
     """
     Calls API, filtering by London, to retrieve London users
     """
-    pass
+    london_users = requests.get('https://bpdts-test-app.herokuapp.com/city/London/users')
+    return all_users.json()
 
-def within_50_miles(user, location_lat, location_lon):
+
+def within_50_miles(user_lat, user_lon, location_lat, location_lon):
     """
-    Takes 'user', an record containing coordinates, and 
+    Takes user coordinates ('user_lat', and 'user_lon') and target
     coordinates ('location_lat' and 'location_lon'), and returns
     a boolean based on whether the user is within 50 miles of the location
     """
-    pass
+    lat_diff = location_lat - user_lat # determines difference in latitude
+    lon_diff = location_lon - user_lon # determines difference in longitude
+    distance = math.sqrt(
+        (lat_diff * lat_diff) + (lon_diff * lon_diff)
+        ) # square root of the lat_diff squared plus the lon_diff squared
+    return distance
+
 
 def get_users_near_london():
     pass
 
+
 def run():
-    pass
+    london_lat = 51.509865
+    london_lon = -0.118092 # taken from 
+    users = get_users()
+    count = 0
+    for u in users:
+        # print(u["latitude"], u["longitude"])
+        distance = within_50_miles(
+            float(u["latitude"]),
+            float(u["longitude"]), # convert from string to float
+            london_lat,
+            london_lon)
+        if distance < 50:
+            count += 1
+    print(count)
+    # print(within_50_miles(
+    #     37.792,-122.419,-33.878, 151.265)*111.139)
+
+
+run()
+
 
 
 
