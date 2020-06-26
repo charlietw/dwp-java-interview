a2057361c8b5dcc1e68f5cf9df3479c7de6e85c7
Charlie <cwren92@gmail.com>
2020-06-26 19:19:46 +0100

    Implement haversine formula and write unit tests to minimise deviation

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import requests # for easy high level API calls, as recommended in Python documentation
-import math # for sqrt function
+from math import radians, cos, sin, asin, sqrt # for sqrt function and radians
+from geopy import distance as geopydistance # for testing
 
 
 def get_instructions():
@@ -10,12 +11,32 @@ def get_instructions():
     return request
 
 
+def user_cleaning(users):
+    """
+    Converts string to float upon retrieval so that it doesn't have to be done
+    in the rest of the code.
+    """
+    for u in users:
+        u["longitude"] = float(u["longitude"])
+        u["latitude"] = float(u["latitude"])
+    return users
+
+
 def get_users():
     """
     Calls API to retrieve all users
     """
     all_users = requests.get('https://bpdts-test-app.herokuapp.com/users')
-    return all_users.json()
+    return all_users
+
+
+def get_users_json():
+    """
+    Retrieves and cleanes users, returning JSON list
+    """
+    all_users = get_users().json()
+    return user_cleaning(all_users)
+
 
 
 def get_london_users():
@@ -23,21 +44,74 @@ def get_london_users():
     Calls API, filtering by London, to retrieve London users
     """
     london_users = requests.get('https://bpdts-test-app.herokuapp.com/city/London/users')
-    return all_users.json()
+    return london_users
 
 
-def within_50_miles(user_lat, user_lon, location_lat, location_lon):
+def calculate_distance(user_lat, user_lon, location_lat, location_lon):
     """
     Takes user coordinates ('user_lat', and 'user_lon') and target
-    coordinates ('location_lat' and 'location_lon'), and returns
-    a boolean based on whether the user is within 50 miles of the location
+    coordinates ('location_lat' and 'location_lon'), and calculates the
+    distance in miles between the two.
+    This function initially used Pythagoras Theorum, but later replaced it with
+    Haversine formula implementation in Python found online as it was more accurate.
     """
-    lat_diff = location_lat - user_lat # determines difference in latitude
-    lon_diff = location_lon - user_lon # determines difference in longitude
-    distance = math.sqrt(
-        (lat_diff * lat_diff) + (lon_diff * lon_diff)
-        ) # square root of the lat_diff squared plus the lon_diff squared
-    return distance
+    # convert decimal degrees to radians 
+    user_lat = radians(user_lat)
+    user_lon = radians(user_lon)
+    location_lat = radians(location_lat)
+    location_lon = radians(location_lon)
+     
+    # haversine formula taken from:
+    # https://kanoki.org/2019/02/14/how-to-find-distance-between-two-points-based-on-latitude-and-longitude-using-python-and-sql/
+    dlon = location_lon - user_lon 
+    dlat = location_lat - user_lat 
+    a = sin(dlat/2)**2 + cos(user_lat) * cos(location_lat) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 3890.0 
+    return c * r
+
+
+def calculate_distance_geopy(user_lat, user_lon, location_lat, location_lon):
+    """
+    Takes user coordinates ('user_lat', and 'user_lon') and target
+    coordinates ('location_lat' and 'location_lon'), and calculates the
+    distance in miles between the two using geopy.
+    """
+    # tuples for geopy
+    user_location = (user_lat, user_lon)
+    london = (location_lat, location_lon)
+    return geopydistance.distance(user_location, london).miles
+
+
+def discrepency_between_calcs(distance_1, distance_2):
+    """
+    Returns discrepency between two distances
+    """
+    return (distance_1 / distance_2) * 100
+
+
+def within_50_miles(user, location_lat, location_lon):
+    """
+    Takes user and returns a boolean based on whether the user
+    is within 50 miles of the location.
+    """
+    distance = calculate_distance(
+        user["latitude"],
+        user["longitude"],
+        location_lat,
+        location_lon,
+        )
+
+    geopy_distance = calculate_distance_geopy(
+        user["latitude"],
+        user["longitude"],
+        location_lat,
+        location_lon,
+        )
+
+    if distance <= 50:
+        return True
+    return False
 
 
 def get_users_near_london():
@@ -45,26 +119,17 @@ def get_users_near_london():
 
 
 def run():
-    london_lat = 51.509865
-    london_lon = -0.118092 # taken from 
-    users = get_users()
+    london_lat, london_lon = 51.509865, -0.118092
+    users = get_users_json()
     count = 0
     for u in users:
-        # print(u["latitude"], u["longitude"])
-        distance = within_50_miles(
-            float(u["latitude"]),
-            float(u["longitude"]), # convert from string to float
+        if within_50_miles(
+            u,
             london_lat,
-            london_lon)
-        if distance < 50:
+            london_lon):
             count += 1
-    print(count)
-    # print(within_50_miles(
-    #     37.792,-122.419,-33.878, 151.265)*111.139)
 
+    print(count)
 
 run()
-
-
-
 
--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import unittest
-from functions import *
 from run import *
+from geopy import distance as geopydistance # for testing
 
 
 class TestInstructions(unittest.TestCase):
@@ -8,15 +8,9 @@ class TestInstructions(unittest.TestCase):
     Asserts that the API can be called successfully
     """
 
-    def setUp(self):
-        pass
-
     def test_api_call(self):
         r = get_instructions()
         self.assertEqual(r.status_code, 200)
-
-    def tearDown(self):
-        pass
 
 
 class TestUsers(unittest.TestCase):
@@ -24,15 +18,9 @@ class TestUsers(unittest.TestCase):
     Asserts that the API can be called successfully to get users
     """
 
-    def setUp(self):
-        pass
-
     def test_api_call(self):
         r = get_users()
         self.assertEqual(r.status_code, 200)
-
-    def tearDown(self):
-        pass
 
 
 class TestLondonUsers(unittest.TestCase):
@@ -40,15 +28,9 @@ class TestLondonUsers(unittest.TestCase):
     Asserts that the API can be called successfully to get users in London
     """
 
-    def setUp(self):
-        pass
-
     def test_api_call(self):
         r = get_london_users()
         self.assertEqual(r.status_code, 200)
-
-    def tearDown(self):
-        pass
 
 
 class TestWithin50Miles(unittest.TestCase):
@@ -57,18 +39,77 @@ class TestWithin50Miles(unittest.TestCase):
     """
 
     def setUp(self):
-        pass
+        self.users = get_users_json()
 
-    def test_true_comparison(self):
-        r = within_50_miles(user, 10, 20) # to be replaced with actual coordinates within 50 miles
-        self.assertEqual(r, True)
 
-    def test_false_comparison(self):
-        r = within_50_miles(user, 10, 20) # to be replaced with actual coordinates outside of 50 miles
-        self.assertEqual(r, False)
+    def test_with_example_data(self):
+        """
+        Takes all positive, whole integers as coordinates and compares with geopy
+        """
+        x_1 = 4
+        y_1 = 1
+        x_2 = 6
+        y_2 = 6
 
-    def tearDown(self):
-        pass
+        p1 = [x_1, y_1]
+        p2 = [x_2, y_2] 
+        distance = calculate_distance(p1[0], p1[1], p2[0], p2[1])
+
+        p1 = (x_1, y_1)
+        p2 = (x_2, y_2)
+        distance_geopy = geopydistance.distance(p1, p2).miles
+        diff = discrepency_between_calcs(distance, distance_geopy)
+        print("distance is ", distance)
+        print("geopy distance is ", distance_geopy)
+        print("difference is ", diff)
+        self.assertTrue(diff < 105)
+        self.assertTrue(diff > 95) # asserts that deviation is less than 5% either side
+
+
+    def test_with_example_data_negative(self):
+        """
+        Takes coordinates and compares with geopy. 
+        Exposes bug when using a negative latitude (fixed).
+        """
+        x_1 = 51.65539
+        y_1 = 0.05725
+        x_2 = 51.509865
+        y_2 = -0.118092
+        p1 = [x_1, y_1]
+        p2 = [x_2, y_2] 
+        distance = calculate_distance(p1[0], p1[1], p2[0], p2[1])
+        p1 = (x_1, y_1) # formatting for geopy
+        p2 = (x_2, y_2)
+        distance_geopy = geopydistance.distance(p1, p2).miles
+        diff = discrepency_between_calcs(distance, distance_geopy)
+        self.assertTrue(diff < 105)
+        self.assertTrue(diff > 95) # asserts that deviation is less than 5% either side
+
+
+    def test_all_data(self):
+        """
+        Gets disrecepency for all data in source and asserts that no
+        difference is > 5% from what is calculated by geopy.
+        """
+        count = 0 # count of inaccurate calculations
+        london_lat = 51.509865
+        london_lon = -0.118092
+        london = (london_lat, london_lon) # formatting for geopy
+        for u in self.users:
+            user_lat = u["latitude"]
+            user_lon = u["longitude"]
+            user = (user_lat, user_lon) # formatting for geopy
+            distance = calculate_distance(user_lat, user_lon, london_lat, london_lon)
+            distance_geopy = geopydistance.distance(user, london).miles # geopy calc
+            diff = discrepency_between_calcs(distance, distance_geopy)
+            if diff > 105 or diff < 95: # if discrepency is > 5% then flag it
+                print("diff:", diff, "coords: ", user)
+                count += 1
+        print("count:", count)
+        self.assertTrue(count == 0) # to pass the test they all have to be < 5%
+
+
+
 
 
 class TestUsersNearLondon(unittest.TestCase):
@@ -76,17 +117,9 @@ class TestUsersNearLondon(unittest.TestCase):
     Asserts that the function to locate users near London returns expected results
     """
 
-    def setUp(self):
-        pass
-
     def get_users_near_london(self):
         r = get_users_near_london() # to be replaced with actual coordinates within 50 miles
         self.assertIsNotNone(r)
-
-
-    def tearDown(self):
-        pass
-
 
 
 
