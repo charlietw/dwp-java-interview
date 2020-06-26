6988b304b7fe749661734a3b6c0f0bb9534a3b32
Charlie <cwren92@gmail.com>
2020-06-26 21:26:51 +0100

    Amend to fit pep8 guidelines

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-import requests # for high level API calls, as recommended in Python documentation
-from math import radians, cos, sin, asin, sqrt # for sqrt function and radians
-from geopy import distance as geopydistance # for testing
+import requests  # for high level API calls, as recommended in Python docs
+from math import radians, cos, sin, asin, sqrt  # for sqrt function and radians
+from geopy import distance as geopydistance  # for testing
 
 
 def get_instructions():
@@ -42,7 +42,8 @@ def get_london_users():
     """
     Calls API, filtering by London, to retrieve London users.
     """
-    london_users = requests.get('https://bpdts-test-app.herokuapp.com/city/London/users')
+    london_users = requests.get(
+        'https://bpdts-test-app.herokuapp.com/city/London/users')
     return london_users
 
 
@@ -53,29 +54,28 @@ def get_london_users_json():
     london_users = get_london_users().json()
     return user_cleaning(london_users)
 
-    
 
 def calculate_distance(user_lat, user_lon, location_lat, location_lon):
     """
     Takes user coordinates ('user_lat', and 'user_lon') and target
     coordinates ('location_lat' and 'location_lon'), and returns the
     distance, in miles, between the two.
-    This function initially used Pythagoras' Theorem, but later replaced it with
-    Haversine formula implementation in Python found online, as it was more accurate.
+    This function initially used Pythagoras' Theorem, but later replaced
+    it with Haversine formula implementation in Python found online, as
+    it was more accurate.
     """
-    # convert decimal degrees to radians 
+    # convert decimal degrees to radians
     user_lat = radians(user_lat)
     user_lon = radians(user_lon)
     location_lat = radians(location_lat)
     location_lon = radians(location_lon)
-     
     # haversine formula taken from:
     # https://kanoki.org/2019/02/14/how-to-find-distance-between-two-points-based-on-latitude-and-longitude-using-python-and-sql/
-    dlon = location_lon - user_lon 
-    dlat = location_lat - user_lat 
+    dlon = location_lon - user_lon
+    dlat = location_lat - user_lat
     a = sin(dlat/2)**2 + cos(user_lat) * cos(location_lat) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    r = 3890.0 
+    c = 2 * asin(sqrt(a))
+    r = 3890.0
     return c * r
 
 
@@ -84,10 +84,10 @@ def calculate_distance_geopy(user_lat, user_lon, location_lat, location_lon):
     Takes user coordinates ('user_lat', and 'user_lon') and target
     coordinates ('location_lat' and 'location_lon'), and calculates the
     distance in miles between the two using geopy.
-    This function is only used for debugging/testing purposes, to ensure accuracy
-    of the 'calculate_distance' function.
+    This function is only used for debugging/testing purposes,
+    to ensure accuracy of the 'calculate_distance' function.
     """
-    user_location = (user_lat, user_lon) # geopy requires tuples
+    user_location = (user_lat, user_lon)  # geopy requires tuples
     london = (location_lat, location_lon)
     return geopydistance.distance(user_location, london).miles
 
@@ -101,7 +101,7 @@ def discrepency_between_calcs(distance_1, distance_2):
 
 def within_50_miles(user_lat, user_lon, location_lat, location_lon):
     """
-    Takes user location and another location and returns a boolean based 
+    Takes user location and another location and returns a boolean based
     on whether the user is within 50 miles of that location.
     """
     distance = calculate_distance(
@@ -121,28 +121,27 @@ def get_users_near_london(users, london_lat, london_lon):
     Function to retrieve users who are either listed as in London,
     or whose current coordinates are within 50 miles of London
     """
-    users_near_london = get_london_users_json() # initial list is the starting point
-    for u in users: # if a user is within 50 miles of London...
+    users_near_london = get_london_users_json()  # initial list
+    for u in users:  # if a user is within 50 miles of London...
         if within_50_miles(
             u["latitude"],
             u["longitude"],
             london_lat,
             london_lon):
-            new_record = True # sets a flag to check against to see if already present
-            for l in users_near_london:
-                if u == l: # If the record is already present in the list...
-                    new_record = False # mark the flag
-                    break # stop looping.
-            if new_record:
-                users_near_london.append(u) # if not, it must be new, therefore append it
-
+                new_record = True  # sets a flag to check against
+                for l in users_near_london:
+                    if u == l:  # If the record is already present...
+                        new_record = False  # mark the flag
+                        break  # stop looping.
+                if new_record:
+                    users_near_london.append(u)  # if not then append
     return users_near_london
 
 
 def run():
-    london_lat, london_lon = 51.509865, -0.118092 # coordinates from www.latlong.net
-    users = get_users_json() # retrieves all users
-    london_users = get_users_near_london( # then calculates which should be returned
+    london_lat, london_lon = 51.509865, -0.118092  # from www.latlong.net
+    users = get_users_json()  # retrieves all users
+    london_users = get_users_near_london(  # then narrows down
         users,
         london_lat,
         london_lon)
@@ -150,4 +149,3 @@ def run():
     return london_users
 
 run()
-
--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import unittest
 from run import *
-from geopy import distance as geopydistance # for testing
+from geopy import distance as geopydistance  # for testing
 
 
 class TestInstructions(unittest.TestCase):
@@ -41,31 +41,28 @@ class TestDistanceCalculation(unittest.TestCase):
     def setUp(self):
         self.users = get_users_json()
 
-
     def test_with_example_data(self):
         """
-        Takes all positive, whole integers as coordinates and compares with geopy
+        Takes all positive, whole integers as coordinates and
+        compares with geopy
         """
         x_1 = 4
         y_1 = 1
         x_2 = 6
         y_2 = 6
-
         p1 = [x_1, y_1]
-        p2 = [x_2, y_2] 
+        p2 = [x_2, y_2]
         distance = calculate_distance(p1[0], p1[1], p2[0], p2[1])
-
         p1 = (x_1, y_1)
         p2 = (x_2, y_2)
         distance_geopy = geopydistance.distance(p1, p2).miles
         diff = discrepency_between_calcs(distance, distance_geopy)
         self.assertTrue(diff < 105)
-        self.assertTrue(diff > 95) # asserts that deviation is less than 5% either side
-
+        self.assertTrue(diff > 95)  # asserts that deviation < 5%
 
     def test_with_example_data_negative(self):
         """
-        Takes coordinates and compares with geopy. 
+        Takes coordinates and compares with geopy.
         Exposes bug when using a negative latitude (fixed).
         """
         x_1 = 51.65539
@@ -73,36 +70,41 @@ class TestDistanceCalculation(unittest.TestCase):
         x_2 = 51.509865
         y_2 = -0.118092
         p1 = [x_1, y_1]
-        p2 = [x_2, y_2] 
+        p2 = [x_2, y_2]
         distance = calculate_distance(p1[0], p1[1], p2[0], p2[1])
-        p1 = (x_1, y_1) # formatting for geopy
+        p1 = (x_1, y_1)  # formatting for geopy
         p2 = (x_2, y_2)
         distance_geopy = geopydistance.distance(p1, p2).miles
         diff = discrepency_between_calcs(distance, distance_geopy)
         self.assertTrue(diff < 105)
-        self.assertTrue(diff > 95) # asserts that deviation is less than 5% either side
-
+        self.assertTrue(diff > 95)  # asserts that deviation is < 5%
 
     def test_all_data(self):
         """
         Gets disrecepency for all data in source and asserts that no
         difference is > 5% from what is calculated by geopy.
         """
-        count = 0 # count of inaccurate calculations
+        count = 0  # count of inaccurate calculations
         london_lat = 51.509865
         london_lon = -0.118092
-        london = (london_lat, london_lon) # formatting for geopy
+        london = (london_lat, london_lon)  # formatting for geopy
         for u in self.users:
             user_lat = u["latitude"]
             user_lon = u["longitude"]
-            user = (user_lat, user_lon) # formatting for geopy
-            distance = calculate_distance(user_lat, user_lon, london_lat, london_lon)
-            distance_geopy = geopydistance.distance(user, london).miles # geopy calc
+            user = (user_lat, user_lon)  # formatting for geopy
+            distance = calculate_distance(
+                user_lat,
+                user_lon,
+                london_lat,
+                london_lon)
+            distance_geopy = geopydistance.distance(
+                user,
+                london).miles  # geopy calc
             diff = discrepency_between_calcs(distance, distance_geopy)
-            if diff > 105 or diff < 95: # if discrepency is > 5% then flag it
+            if diff > 105 or diff < 95:  # if discrepency is > 5% then flag it
                 count += 1
         print("count:", count)
-        self.assertTrue(count == 0) # to pass the test they all have to be < 5%
+        self.assertTrue(count == 0)  # to pass the test they all must be < 5%
 
 
 class TestWithin50Miles(unittest.TestCase):
@@ -122,7 +124,6 @@ class TestWithin50Miles(unittest.TestCase):
             lat_1, lon_1, lat_2, lon_2)
         )
 
-
     def test_true_with_identical(self):
         """
         Tests with two identical coordinates
@@ -134,7 +135,6 @@ class TestWithin50Miles(unittest.TestCase):
         self.assertTrue(within_50_miles(
             lat_1, lon_1, lat_2, lon_2)
         )
-
 
     def test_false(self):
         """
@@ -149,16 +149,15 @@ class TestWithin50Miles(unittest.TestCase):
         )
 
 
-
 class TestUsersNearLondon(unittest.TestCase):
     """
-    Asserts that the function to locate users near London returns expected results
+    Asserts that the function to locate users near
+    London returns expected results.
     """
 
     def get_users_near_london(self):
-        r = get_users_near_london() 
+        r = get_users_near_london()
         self.assertIsNotNone(r)
-
 
 if __name__ == '__main__':
     unittest.main()
